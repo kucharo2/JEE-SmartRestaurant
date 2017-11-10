c68f0df5afdaf06f1dc58abb254de98eda3f56a8
Pavel Matyáš <matyapav@fel.cvut.cz>
2017-11-10 18:19:26 +0100

    fe update - boostrap added, displaying all items, item detail and right combinations.

--- src/main/java/cz/kucharo2/rest/MenuEndpoint.java
+++ src/main/java/cz/kucharo2/rest/MenuEndpoint.java
@@ -1,5 +1,6 @@
 package cz.kucharo2.rest;
 
+import cz.kucharo2.data.dao.ItemDao;
 import cz.kucharo2.data.entity.Category;
 import cz.kucharo2.data.entity.Item;
 import cz.kucharo2.data.enums.CategoryType;
@@ -24,35 +25,14 @@ public class MenuEndpoint {
     @Inject
     private MenuService menuService;
 
+    @Inject
+    private ItemDao itemDao;
+
     @GET
     @Path("items")
     @Produces(MediaType.APPLICATION_JSON)
     public List<Item> getAllMenuItems() {
-        List<Item> itemsList = new ArrayList<>();
-
-        Category category = new Category();
-        category.setName("Hotovky");
-        category.setCode(CategoryType.HLAVNI_JIDLO);
-
-        Item item1 = new Item();
-        item1.setId(1);
-        item1.setCategory(category);
-        item1.setName("Smažený sýr");
-        item1.setCode("120");
-        item1.setDescription("S eidamem to je jako se ženskými... třicítka je lepší jak pětačtyřicítka");
-        item1.setPrice(100);
-
-        Item item2 = new Item();
-        item2.setId(2);
-        item2.setCategory(category);
-        item2.setName("Krkovička");
-        item2.setCode("121");
-        item2.setDescription("Chro chro ...");
-        item2.setPrice(150);
-
-        itemsList.add(item1);
-        itemsList.add(item2);
-        return itemsList;
+        return itemDao.getAll();
     }
 
     @GET
--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -14,4 +14,23 @@ app.controller('MenuListController', function MenuListController($scope, $http) 
         .then(function (response) {
             $scope.items = response.data;
         });
+
+    $scope.fetchDetailOfItem = function (id) {
+        for (var index = 0; index < $scope.items.length; index++) {
+            if (id === $scope.items[index].id) {
+                $scope.detailItem = $scope.items[index];
+                fetchCombinationsForItem(id);
+                return;
+            }
+        }
+    }
+
+    var fetchCombinationsForItem = function (id) {
+        $http.get(apiPrefix + "/menu/item/" + id + "/sideDish")
+            .then(function (response) {
+                $scope.sideDishes = response.data;
+            });
+
+    }
+
 });
