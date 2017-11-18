8923b71ec33f89e5b9afa6b737e0b3f25b563b38
rkuchar <kucharrom@gmail.com>
2017-11-18 19:27:53 +0100

    arquillian updates

--- src/test/java/cz/kucharo2/data/dao/impl/BillItemImplTest.java
+++ src/test/java/cz/kucharo2/data/dao/impl/BillItemImplTest.java
@@ -1,10 +1,13 @@
 package cz.kucharo2.data.dao.impl;
 
+import cz.kucharo2.data.dao.BillItemDao;
+import cz.kucharo2.data.entity.BillItem;
+import cz.kucharo2.data.enums.BillStatus;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.asset.EmptyAsset;
-import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -16,9 +19,11 @@ import org.junit.runner.RunWith;
 public class BillItemImplTest {
 
     @Deployment
-    public static JavaArchive createDeployment() {
-        return ShrinkWrap.create(JavaArchive.class)
-                .addClass(BillItemImpl.class)
+    public static WebArchive createDeployment() {
+        return ShrinkWrap.create(WebArchive.class)
+//                .addPackages(true, BillItemDao.class.getPackage())
+//                .addPackages(true, BillItem.class.getPackage())
+//                .addPackages(true, BillStatus.class.getPackage())
                 .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
     }
 
