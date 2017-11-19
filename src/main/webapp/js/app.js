227ace2aff588988cd51067b48c9846be6435a01
Pavel Matyáš <matyapav@fel.cvut.cz>
2017-11-19 14:24:55 +0100

    fe - routing nad notfound page

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -3,9 +3,22 @@
  * @type {angular.Module}
  */
 
-var app = angular.module('smartRestaurantApp', ['base64', 'ngMaterial', 'ngAria']).config(function($httpProvider, $base64) {
+var app = angular.module('smartRestaurantApp', ['ngRoute', 'base64', 'ngMaterial', 'ngAria']).config(function($routeProvider, $httpProvider, $base64) {
     var auth = $base64.encode("anonymous:cvut2017");
     $httpProvider.defaults.headers.common.Authorization = 'Basic ' + auth;
+    $routeProvider
+        .when("/", {
+            templateUrl : "offer.html"
+        })
+        .when("/login", {
+            templateUrl : "login.html"
+        })
+        .when("/tables", {
+            templateUrl : "tables.html"
+        })
+        .otherwise({
+            templateUrl : "notFound.html"
+        });
 });
 var apiPrefix = "api/v1";
 
